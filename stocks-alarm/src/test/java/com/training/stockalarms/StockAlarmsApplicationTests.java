1866084ff919a3539d4d5018929a5bd064dad415
AndreiZahan <zahan.andrei95@gmail.com>
2021-08-15 08:18:37 +0300

    added project arhitecture (maven & spring)

---
+++ stocks-alarm/src/test/java/com/training/stockalarms/StockAlarmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.stockalarms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAlarmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
